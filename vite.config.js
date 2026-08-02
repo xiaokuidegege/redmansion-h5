a07be67d78f0f4aeb3f80dd545687a82fe96e9a8
xiaokuidegege <xiaokuidegege@gmail.com>
2026-08-02 13:41:55 +0800

    add github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   base: '/redmansion-h5/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        loading: resolve(__dirname, 'loading.html'),
+      },
+    },
+  },
 })
